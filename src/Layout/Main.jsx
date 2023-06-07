f317d043be1a64d5898f6007608254b976ddd301
dipto3478 <kitkatboy3478@gmail.com>
2023-06-07 21:11:26 +0600

    footer added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../Shared/NavBar/NavBar";
+import Footer from "../Shared/Footer/Footer";
 
 const Main = () => {
   return (
     <>
       <NavBar />
       <Outlet />
+      <Footer />
     </>
   );
 };
---
+++ src/Shared/ErrorPages/ErrorPages.jsx
@@ -0,0 +1,5 @@
+const ErrorPages = () => {
+  return <div></div>;
+};
+
+export default ErrorPages;
