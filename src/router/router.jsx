c38197cb197333ab43c0a7af79c1280dfa6d25d1
dipto3478 <kitkatboy3478@gmail.com>
2023-06-07 21:29:46 +0600

    error page done

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../pages/Home/Home";
+import ErrorPages from "../Shared/ErrorPages/ErrorPages";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
+    errorElement: <ErrorPages />,
     children: [
       {
         path: "/",
